c6f8b32bc099a60f6b74cf4298712e6a6ab82623
NodeRunnerUA <118058348+Vladymirovich@users.noreply.github.com>
2025-02-11 12:12:14 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,12 @@ const PORT = process.env.PORT || 3000;
 
 console.log("Starting CryptoHawk project...");
 
-// Запускаем все боты (каждый бот должен использовать уникальный Telegram-токен)
-// Admin Bot (например, bots/adminBot.js)
+// Запускаем все боты (каждый бот должен использовать уникальный токен)
 require('./bots/adminBot');
-
-// MarketStats Bot (например, bots/marketStatsBot.js)
-// В этом файле должен присутствовать вызов: require('../MarketStats/poller') для запуска опроса API
 require('./bots/marketStatsBot');
+require('./bots/cexBot');
 
-// Если есть и другие боты (например, CEX Bot), можно добавить:
-// require('./bots/cexBot');
-
-// Поднимаем простой HTTP-сервер для keep-alive (необходим Railway.app)
+// Запускаем простой HTTP-сервер для keep-alive (важно для Railway)
 app.get('/', (req, res) => {
   res.send('CryptoHawk bots are running.');
 });
