a6fffe37891f1dcd60ed89635e67167b5180209e
NodeRunnerUA <118058348+Vladymirovich@users.noreply.github.com>
2025-02-13 18:19:58 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config({ path: './config/.env' });
+const axios = require('axios');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
