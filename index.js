3d179301ac4181bcfb74afad9bb3311b9a5db37e
NodeRunnerUA <118058348+Vladymirovich@users.noreply.github.com>
2025-02-13 13:19:59 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 require('dotenv').config({ path: './config/.env' });
 const express = require('express');
 const app = express();
@@ -6,16 +5,9 @@ const PORT = process.env.PORT || 3000;
 
 console.log("Starting CryptoHawk project...");
 
-// Запускаем все боты (каждый бот должен использовать уникальный токен)
 require('./bots/adminBot');
 require('./bots/marketStatsBot');
 require('./bots/cexBot');
 
-// Запускаем простой HTTP-сервер для keep-alive (важно для Railway)
-app.get('/', (req, res) => {
-  res.send('CryptoHawk bots are running.');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.get('/', (req, res) => res.send('CryptoHawk bots are running.'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
